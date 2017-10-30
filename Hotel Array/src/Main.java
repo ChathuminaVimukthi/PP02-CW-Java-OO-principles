3f48cb19ceccc02e0545f976caf2e9aa5f10b35c
Yohansilva123 <33213988+Yohansilva123@users.noreply.github.com>
2017-10-30 16:42:46 +0530

    Update Main.java

--- Hotel Array/src/Main.java
+++ Hotel Array/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	public static void main(String[] args){
 		
 		
-		String Customer = null ;
+		String Customer = null ; //set customer as null
 		String menu = null;
 		int roomNo = 0;
 		
